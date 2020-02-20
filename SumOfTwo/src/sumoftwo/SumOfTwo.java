a62afe986e7c6e85356ef5d841e0dcfc928cef7b
shura08g <shura08g@rambler.ru>
2020-02-20 13:04:37 +0200

    Update SumOfTwo.java

--- SumOfTwo/src/sumoftwo/SumOfTwo.java
+++ SumOfTwo/src/sumoftwo/SumOfTwo.java
@@ -22,16 +22,17 @@ public class SumOfTwo {
         int[] a = {1, 2, 3};
         int[] b = {10, 20, 30, 40};
         int v = 42;
+        int[] a2 = {0, 0, -5, 30212};
+        int[] b2 = {-10, 40, -3, 9};
         
+        //long start = System.nanoTime();
+        //start = System.currentTimeMillis();
         System.out.println("sumOfTwo(a, b, v)");
         System.out.println(sumOfTwo(a, b, v));  //true
         System.out.println(sumOfTwo(a, b, 55)); //false
-        
-        int[] a2 = {0, 0, -5, 30212};
-        int[] b2 = {-10, 40, -3, 9};
         System.out.println(sumOfTwo(a2, b2, -8));  //true
         System.out.println(sumOfTwo(a2, b2, -9));  //false
-        
+       
         System.out.println("-----------------");
         System.out.println("sumOfTwo2(a, b, v)");
         System.out.println(sumOfTwo2(a, b, 42));  //true
@@ -46,13 +47,42 @@ public class SumOfTwo {
         System.out.println(sumOfTwoHashSet(a2, b2, -8));  //true
         System.out.println(sumOfTwoHashSet(a2, b2, -9));  //false
         
+        
+        int[] a3 = {0, 0, -5, 30212, 325, 548, 6, 47, 4, 8, 7, 7, 89, 7, 8, 4, 7, 6, 4, 7, 9, 6594, 65, 78488, 48};
+        int[] b3 = {-10, 40, -3, 9, -5, 3, 5, 4, 96, -87, -566, 54, 4, -654487, -6594, -5648, -6594, -654484 ,-254, 6548, 484 ,567, 74989};
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 1000000; i++) {
+            sumOfTwo(a3, b3, 42);
+        }
+        long finish = System.currentTimeMillis();
+        long timeConsumedMillis = finish - start;
+        System.out.println(timeConsumedMillis);
+        
+        start = System.currentTimeMillis();
+        for (int i = 0; i < 1000000; i++) {
+            sumOfTwo2(a3, b3, 42);
+        }
+        finish = System.currentTimeMillis();
+        timeConsumedMillis = finish - start;
+        System.out.println(timeConsumedMillis);
+        
+        start = System.currentTimeMillis();
+        for (int i = 0; i < 1000000; i++) {
+            sumOfTwoHashSet(a3, b3, 42);
+        }
+        finish = System.currentTimeMillis();
+        timeConsumedMillis = finish - start;
+        System.out.println(timeConsumedMillis);
+        
+        
+        
     }
     
     public static boolean sumOfTwo(int[] a, int[] b, int v){
         for (int ai : a){
             for (int bi : b){
                 if (ai + bi == v) {
-                    System.out.println(ai + " + " + bi + " = " + v);
+                    //System.out.println(ai + " + " + bi + " = " + v);
                     return true;
                 }
             }
@@ -65,7 +95,7 @@ public class SumOfTwo {
             int needed_value = v - a[i];
             for (int j = 0; j < b.length; j++){
                 if (b[j] == needed_value) {
-                    System.out.println(a[i] + " + " + b[j] + " = " + v);
+                    //System.out.println(a[i] + " + " + b[j] + " = " + v);
                     return true;
                 }
             }
