ef5c9525f644b3c35e38979557a1bd0d6cbcf8dd
shura08g <shura08g@rambler.ru>
2020-07-07 13:30:13 +0300

    Add files via upload

---
+++ ThinkingInJava/src/thinkinginjavach21/AtomicEvenGenerator.java
@@ -0,0 +1,16 @@
+// 32 Многопоточность
+package thinkinginjavach21;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class AtomicEvenGenerator extends IntGenerator {
+    private AtomicInteger currentEvenValue = new AtomicInteger(0);
+    
+    public int next() {
+        return currentEvenValue.addAndGet(2);
+    }
+    
+    public static void main(String[] args) {
+        EvenChecker.test(new AtomicEvenGenerator());
+    }
+}
